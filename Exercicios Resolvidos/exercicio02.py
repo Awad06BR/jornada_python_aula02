7d397bc5f89ef2b9163349aceffab2cc5a2d40ea
Miguel <contatoawad@outlook.com>
2026-01-14 08:10:03 -0300

    Exercicio 02 - Resolvido

---
+++ Exercicios Resolvidos/exercicio02.py
@@ -0,0 +1,7 @@
+# 2. Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
+
+numero =int(input("Digite um numero: "))
+
+resto_divisao = numero % 5 
+
+print(f"o resto da divisão do numero digitado por 5 e igual a: {resto_divisao}")
