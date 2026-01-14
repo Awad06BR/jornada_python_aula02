02d7fa2ece57262bfd5e4521becc9b0ef6853f00
Miguel <contatoawad@outlook.com>
2026-01-14 08:02:35 -0300

    Exercicio 01 Resolvido

---
+++ Exercicios Resolvidos/exercicio01.py
@@ -0,0 +1,9 @@
+# 1. Escreva um programa que soma dois números inteiros inseridos pelo usuário.
+
+
+numero1 = int(input("Digite o primeiro numero: "))
+numero2 = int(input("Digite o segundo numero: "))
+
+soma = numero1 + numero2
+
+print(f"A soma dos numeros digitados e igual a: {soma}")
