3a2ce2dcaa2dbf7d3d523eee720d6b9f65da55b2
Miguel <contatoawad@outlook.com>
2026-01-14 08:30:28 -0300

    Exercicio 05 - Resolvido (lista de exercicios Int finalizada)

---
+++ Exercicios Resolvidos/exercicio05.py
@@ -0,0 +1,6 @@
+# 5. Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
+numero1 = int(input("Digite um numero: "))
+
+aoQuadrado = numero1 ** 2
+
+print(f"O numeros fornecidos ao quadrado e igual a: {aoQuadrado}")
