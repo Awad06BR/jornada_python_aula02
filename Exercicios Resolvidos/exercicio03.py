9d48b09fc5cc15b8cb78e0a171f417d41ce84bb9
Miguel <contatoawad@outlook.com>
2026-01-14 08:15:39 -0300

    Exercicio 03 - Resolvido

---
+++ Exercicios Resolvidos/exercicio03.py
@@ -0,0 +1,9 @@
+# 3. Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
+
+numero1 = int(input("Digite o primeiro numero: "))
+numero2 = int(input("Digite o segundo numero: "))
+
+multiplicacao = numero1*numero2
+
+print(f"A multiplicação dos numeros fornecidos e igual a: {multiplicacao}")
+
